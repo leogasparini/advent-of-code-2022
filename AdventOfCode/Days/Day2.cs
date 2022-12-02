0a0f53bcc150c515ca89de340f8a93e0761fabe1
Leonardo Gasparini <leonardo.gasparini@musement.com>
2022-12-02 15:40:44 -0500

    complete day 2

--- AdventOfCode/Days/Day2.cs
+++ AdventOfCode/Days/Day2.cs
@@ -11,6 +11,9 @@ internal abstract class Move
 {
     protected abstract int GetMoveScore();
     protected abstract int GetOutcomeScore(Move opponentMove);
+    public abstract Move GenerateLosingMove();
+    public abstract Move GenerateWinningMove();
+    public Move GenerateDrawMove() => this;
 
     public int GetRoundScore(Move opponentMove) => GetMoveScore() + GetOutcomeScore(opponentMove);
 }
@@ -25,6 +28,9 @@ internal sealed class Rock : Move
         Paper => OutcomeScores.Loss,
         Scissor => OutcomeScores.Win
     };
+
+    public override Move GenerateLosingMove() => new Scissor();
+    public override Move GenerateWinningMove() => new Paper();
 }
 
 internal sealed class Paper : Move
@@ -37,6 +43,9 @@ internal sealed class Paper : Move
         Paper => OutcomeScores.Draw,
         Scissor => OutcomeScores.Loss
     };
+
+    public override Move GenerateLosingMove() => new Rock();
+    public override Move GenerateWinningMove() => new Scissor();
 }
 
 internal sealed class Scissor : Move
@@ -49,6 +58,9 @@ internal sealed class Scissor : Move
         Paper => OutcomeScores.Win,
         Scissor => OutcomeScores.Draw,
     };
+
+    public override Move GenerateLosingMove() => new Paper();
+    public override Move GenerateWinningMove() => new Rock();
 }
 
 internal static class MoveCodes
@@ -61,23 +73,18 @@ internal static class MoveCodes
     public const string PlayerScissors = "Z";
 }
 
+internal static class StrategyCodes
+{
+    public const string Lose = "X";
+    public const string Draw = "Y";
+    public const string Win = "Z";
+}
+
 public class Day2 : AdventOfCodeDay
 {
     protected override int GetDay() => 2;
 
     protected override string GetTask1Solution()
-    {
-        return GetScores()
-            .Sum()
-            .ToString();
-    }
-
-    protected override string GetTask2Solution()
-    {
-        return base.GetTask2Solution();
-    }
-
-    private static IEnumerable<int> GetScores()
     {
         string inputPath = Path.Combine("Assets/day2.txt");
 
@@ -89,7 +96,34 @@ public class Day2 : AdventOfCodeDay
                 Move playerMove = ParseMove(moves[1]);
 
                 return playerMove.GetRoundScore(opponentMove);
-            });
+            })
+            .Sum()
+            .ToString();
+    }
+
+    protected override string GetTask2Solution()
+    {
+        string inputPath = Path.Combine("Assets/day2.txt");
+
+        return File.ReadLines(inputPath)
+            .Select(round =>
+            {
+                string[] moves = round.Split(' ');
+                Move opponentMove = ParseMove(moves[0]);
+                Move playerMove = GetStrategicMove(opponentMove, moves[1]);
+                
+                return playerMove.GetRoundScore(opponentMove);
+            })
+            .Sum()
+            .ToString();
+
+        Move GetStrategicMove(Move opponentMove, string strategy) => strategy switch
+        {
+            StrategyCodes.Lose => opponentMove.GenerateLosingMove(),
+            StrategyCodes.Draw => opponentMove.GenerateDrawMove(),
+            StrategyCodes.Win => opponentMove.GenerateWinningMove(),
+            _ => throw new ArgumentException("Invalid strategy")
+        };
     }
 
     private static Move ParseMove(string rawMove) => rawMove switch
