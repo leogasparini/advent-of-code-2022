0011b72c147c704076a3836db24888fb37c096c3
Leonardo Gasparini <leonardo.gasparini@musement.com>
2022-12-03 09:15:30 -0500

    added elapsed ticks for each solution

--- AdventOfCode/Days/AdventOfCodeDay.cs
+++ AdventOfCode/Days/AdventOfCodeDay.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Linq.Expressions;
+
 namespace AdventOfCode.Days;
 
 public abstract class AdventOfCodeDay
@@ -8,10 +11,23 @@ public abstract class AdventOfCodeDay
 
     public void PrintSolutions()
     {
+        (string solution1, long elapsedTicks1) = GetSolutionAndElapsedTicks(GetTask1Solution);
+        (string solution2, long elapsedTicks2) = GetSolutionAndElapsedTicks(GetTask2Solution);
+        
         int day = GetDay();
         Console.WriteLine("Day {0}: https://adventofcode.com/2022/day/{0}", day);
-        Console.WriteLine("  Task 1: {0}", GetTask1Solution());
-        Console.WriteLine("  Task 2: {0}", GetTask2Solution());
+        Console.WriteLine("  Task 1: {0} (elapsed ticks: {1})", solution1, elapsedTicks1);
+        Console.WriteLine("  Task 2: {0} (elapsed ticks: {1})", solution2, elapsedTicks2);
         Console.WriteLine();
+    }
+
+    private (string, long) GetSolutionAndElapsedTicks(Func<string> solutionFunction)
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        string solution = solutionFunction();
+        watch.Stop();
+        long elapsedTicks = watch.ElapsedTicks;
+
+        return (solution, elapsedTicks);
     }
 }
