20e1e90d93161ed5bfd48f4d15cce66a4aaa8d4c
Leonardo Gasparini <leonardo.gasparini@musement.com>
2022-12-04 09:07:16 -0500

    day 4

--- AdventOfCode/Days/Day4.cs
+++ AdventOfCode/Days/Day4.cs
@@ -3,4 +3,49 @@ namespace AdventOfCode.Days;
 public class Day4 : AdventOfCodeDay
 {
     protected override int GetDay() => 4;
+
+    protected override string GetTask1Solution()
+    {
+        string inputPath = Path.Combine("Assets", "day4.txt");
+
+        int count = 0;
+        foreach (string lines in File.ReadLines(inputPath))
+        {
+            IEnumerable<int[]> sections = lines.Split(',')
+                .Select(s => s.Split('-').Select(int.Parse).ToArray());
+
+            int[] group1 = sections.First();
+            int[] group2 = sections.Last();
+
+            if ((group1[0] <= group2[0] && group1[1] >= group2[1])
+                || (group2[0] <= group1[0] && group2[1] >= group1[1]))
+            {
+                count++;
+            }
+        }
+
+        return count.ToString();
+    }
+    
+    protected override string GetTask2Solution()
+    {
+        string inputPath = Path.Combine("Assets", "day4.txt");
+
+        int count = 0;
+        foreach (string lines in File.ReadLines(inputPath))
+        {
+            IEnumerable<int[]> sections = lines.Split(',')
+                .Select(s => s.Split('-').Select(int.Parse).ToArray());
+
+            int[] group1 = sections.First();
+            int[] group2 = sections.Last();
+
+            if (group1[0] <= group2[1] && group2[0] <= group1[1])
+            {
+                count++;
+            }
+        }
+
+        return count.ToString();
+    }
 }
