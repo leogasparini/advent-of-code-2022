fbaa0228eeb87abb5315484a0b49e734838cbb37
Leonardo Gasparini <leonardo.gasparini@musement.com>
2022-12-24 09:49:23 -0500

    day 9

--- src/Days/Day9.cs
+++ src/Days/Day9.cs
@@ -3,4 +3,127 @@ namespace AdventOfCode.Days;
 public class Day9 : AdventOfCodeDay
 {
     protected override int GetDay() => 9;
+
+    protected override string GetTask1Solution()
+    {
+        string inputPath = Path.Combine("Assets", "day9.txt");
+        HashSet<Knot> visitedPositions = new();
+        Rope rope = new(2);
+
+        foreach (string line in File.ReadLines(inputPath))
+        {
+            (string direction, int steps) = ParseInstructions(line);
+
+            for (int i = 0; i < steps; i++)
+            {
+                rope.MoveHead(direction);
+                visitedPositions.Add(rope.Tail);
+            }
+        }
+
+        return visitedPositions.Count.ToString();
+    }
+
+    protected override string GetTask2Solution()
+    {
+        string inputPath = Path.Combine("Assets", "day9.txt");
+        HashSet<Knot> visitedPositions = new();
+        Rope rope = new(10);
+
+        foreach (string line in File.ReadLines(inputPath))
+        {
+            (string direction, int steps) = ParseInstructions(line);
+
+            for (int i = 0; i < steps; i++)
+            {
+                rope.MoveHead(direction);
+                visitedPositions.Add(rope.Tail);
+            }
+        }
+
+        return visitedPositions.Count.ToString();
+    }
+
+    private (string direction, int steps) ParseInstructions(string line)
+    {
+        string[] instructions = line.Split(' ');
+
+        return (instructions.First(), int.Parse(instructions.Last()));
+    }
+
+    private sealed record Knot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    
+    private sealed class Rope
+    {
+        private readonly Queue<Knot> _knots = new();
+        
+        public Knot Tail => _knots.Last();
+
+        public Rope(int knotsCount)
+        {
+            for (int i = 0; i < knotsCount; i++)
+            {
+                _knots.Enqueue(new Knot());
+            }
+        }
+
+        public void MoveHead(string direction)
+        {
+            Knot head = _knots.First();
+            
+            switch (direction)
+            {
+                case "U":
+                    head.Y++;
+                    break;
+                case "R":
+                    head.X++;
+                    break;
+                case "D":
+                    head.Y--;
+                    break;
+                case "L":
+                    head.X--;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid move");
+            }
+
+            MoveNextKnot(head, 1);
+        }
+
+        private void MoveNextKnot(Knot movedKnot, int nextIndex)
+        {
+            Knot nextKnot = _knots.ElementAt(nextIndex);
+            int distanceX = Math.Abs(movedKnot.X - nextKnot.X);
+            int distanceY = Math.Abs(movedKnot.Y - nextKnot.Y);
+
+            if (distanceX > 1)
+            {
+                nextKnot.X += distanceX / (movedKnot.X - nextKnot.X);
+
+                if (distanceY == 1)
+                {
+                    nextKnot.Y += distanceY / (movedKnot.Y - nextKnot.Y);
+                }
+            }
+
+            if (distanceY > 1)
+            {
+                nextKnot.Y += distanceY / (movedKnot.Y - nextKnot.Y);
+
+                if (distanceX == 1)
+                {
+                    nextKnot.X += distanceX / (movedKnot.X - nextKnot.X);
+                }
+            }
+
+            if (_knots.Count - 1 > nextIndex)
+                MoveNextKnot(nextKnot, nextIndex + 1);
+        }
+    }
 }
--- tests/Days/Day9Tests.cs
+++ tests/Days/Day9Tests.cs
@@ -7,7 +7,7 @@ public class Day9Tests : Day9
     [Fact]
     public void GetTask1Solution_ShouldReturnExpectedSolution()
     {
-        string expected = "TODO";
+        string expected = "6503";
         string actual = GetTask1Solution();
         
         Assert.Equal(expected, actual);
@@ -16,7 +16,7 @@ public class Day9Tests : Day9
     [Fact]
     public void GetTask2Solution_ShouldReturnExpectedSolution()
     {
-        string expected = "TODO";
+        string expected = "2724";
         string actual = GetTask2Solution();
         
         Assert.Equal(expected, actual);
